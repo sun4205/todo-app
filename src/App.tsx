27aba099337936148334712cce61db2a9c10d398
sun4205 <hanna4205@gmail.com>
2026-03-17 18:26:10 -0400

    add basic todo app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import { useState } from 'react'
+import Todolist from './Component/Todolist'
 import './App.css'
 
 function App() {
+  const [todos,setTodos] = useState<string[]>([]);
+  const [input,setInput] = useState('');
+
+  const addTodo = () => {
+    if(input.trim()==='') return;
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
+  const removeTodo = (index:number) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  }
 
   return (
-    <>
-          </>
+    <div className="app">
+    <h1>My Todo App</h1>
+    <div className="input-area">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a todo"
+      />
+      <button onClick={addTodo}>Add</button>
+    </div>
+    <Todolist todos={todos} removeTodo={removeTodo} />
+  </div>
   )
 }
 
